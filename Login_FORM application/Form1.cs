c4941de6a22c0289167a5aac6ca1a7701e3abf9b
Dumre Sabin <70896989+dumresarkar@users.noreply.github.com>
2022-07-04 21:39:32 +0545

    Login_Window_form
    Created a simple Window login form using c# in visual studio 2022

---
+++ Login_FORM application/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Login_FORM_application
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SqlConnection conn = new SqlConnection(@"Data Source=DESKTOP-IS83BVQ;Initial Catalog=Database001;Integrated Security=True");
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_login_Click(object sender, EventArgs e)
+        {
+            string username, user_password;
+
+            username = txt_username.Text;
+            user_password = txt_password.Text;
+
+            try
+            {
+                string querry = "SELECT * FROM Login_wf WHERE username ='"+txt_username.Text+"' AND password='"+txt_password.Text+"'";
+                  
+                
+                SqlDataAdapter sda = new SqlDataAdapter(querry, conn);
+
+                DataTable dtable = new DataTable();
+                sda.Fill(dtable);
+
+                if(dtable.Rows.Count > 0)
+                {
+                    username = txt_username.Text;
+                    user_password = txt_password.Text;
+
+                    //page that needed to be load next 
+                    Menuform form2 = new Menuform();
+                    form2.Show();
+                    this.Hide();
+                }
+                else
+                {
+                    MessageBox.Show("Invalid login details", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    txt_username.Clear();
+                    txt_password.Clear();
+
+                    //to focus username 
+                    txt_username.Focus();
+                }
+
+
+
+            }
+            catch
+            {
+                MessageBox.Show("Error ncounter");
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        private void button_clear_Click(object sender, EventArgs e)
+        {
+            txt_username.Clear();
+            txt_password.Clear();
+
+            txt_username.Focus();
+        }
+
+        private void button_exit_Click(object sender, EventArgs e)
+        {
+            DialogResult res;
+            res = MessageBox.Show("Do you want to exit", "Exit",MessageBoxButtons.YesNo,MessageBoxIcon.Question);
+            if(res == DialogResult.Yes)
+            {
+                Application.Exit();
+
+            }
+            else
+            {
+                this.Show();
+            }
+        }
+    }
+}
